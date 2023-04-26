916f10ba6dfaeaf67e9b5bcba993f9d0e3d9afbb
Valeria Oreshko <oreshko-v@yandex.ru>
2023-04-26 19:16:35 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        int start = 100; //
-        int plus = 1100;
+        int start = 100; //начальная сумма счета
+        int plus = 1100; //сумма пополения
 
-        int bonus;
+        int bonus; //сумма бонусов
         if (plus > 1000) {
             bonus = plus / 100;
         } else {
             bonus = 0;
         }
 
-        int result = bonus + start + plus;
+        int result = bonus + start + plus; //итоговая сумма
 
         System.out.println("Бонусы " + bonus);
         System.out.println("Итоговый счет " + result);
